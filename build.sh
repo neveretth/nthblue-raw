4066dd9344bf743a961150f19e2dd4bca648523b
N. Everett Howard <howard@nth.blue>
2025-06-03 13:27:17 -0400

    Add simple styles to html.

--- build.sh
+++ build.sh
@@ -7,13 +7,29 @@
 # dir/dir/name/index.html
 # so that it appears clean in nginx webhosting.
 # The binary used for conversion is specified by $md2html_bin
+# The style specified by css_style (single css file) is added
+# to every html file.
 # **********************************
 
 raw_data_dir=raw
 output_dir=build
 md2html_bin=/bin/md2html
+css_style=styles/standard.css
 
 file_extention="*.md"
+
+html_prefix='
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="main.css">
+</head>
+<body>
+'
+html_suffix='
+</body>
+</html>
+'
 
 files=$(find $raw_data_dir -name "$file_extention" ) 
 
@@ -25,5 +41,8 @@ do
   output_location="$(echo $output_location | sed -e "s/raw\///g")" # remove raw/
   output_location="$(echo $output_location | sed -e "s/index//g")" # remove index dir path (if file is an index.md)
   mkdir -p $output_location 
-  $md2html_bin $file > $output_location/index.html
+  echo $html_prefix > $output_location/index.html
+  $md2html_bin $file >> $output_location/index.html
+  echo $html_suffix >> $output_location/index.html
+  cp $css_style $output_location/main.css
 done
